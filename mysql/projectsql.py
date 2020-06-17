6b2d11953fd3f7e0fea7a7db1fd6755f3142410c
VishakAmin <aminvishak@gmail.com>
2020-06-17 23:33:23 +0530

    Reviewed

--- mysql/projectsql.py
+++ mysql/projectsql.py
@@ -1,15 +1,17 @@
 import mysql.connector
 con = mysql.connector.connect(
-    user = "ardit700_student",
-    password = "ardit700_student",
-    host = "108.167.140.122",
-    database = "ardit700_pm1database"
+    user="ardit700_student",
+    password="ardit700_student",
+    host="108.167.140.122",
+    database="ardit700_pm1database"
 )
+
 cursor = con.cursor()
 
 w = input("Enter The Word: ")
-query = cursor.execute("SELECT * FROM Dictionary WHERE EXPRESSION = '%s'"%w)
+query = cursor.execute("SELECT * FROM Dictionary WHERE EXPRESSION = '%s'" % w)
 r = cursor.fetchall()
+
 
 def transfer(results):
     if results:
